5223fa98d075e609d124311abfef5c825630a305
Steve Houser <steve@damconsortium.com>
2012-09-21 18:20:09 -0600

    Modified to use DAMConsortium git repo of brewstrap

--- brewstrap.sh
+++ brewstrap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-BREWSTRAP_BASE="https://github.com/schubert/brewstrap"
+BREWSTRAP_BASE="https://github.com/DAMConsortium/brewstrap"
 PLATFORM=`uname -s`
 BREWSTRAP_BIN="${BREWSTRAP_BASE}/raw/master/brewstrap_${PLATFORM}.sh"
 
 curl -fsSL "${BREWSTRAP_BIN}" > /tmp/${USER}-brewstrap_${PLATFORM}.sh
 
 exec bash /tmp/${USER}-brewstrap_${PLATFORM}.sh
-
