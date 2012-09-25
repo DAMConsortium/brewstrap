a1739ae48dc4100ad487434e87935f1b5a610226
Steve Houser <steve@damconsortium.com>
2012-09-24 19:47:36 -0600

    Continued modifying to handle different darwin environments and ubiquity needs.

--- brewstrap_Darwin.sh
+++ brewstrap_Darwin.sh
@@ -3,10 +3,9 @@
 BREWSTRAPRC="${HOME}/.brewstraprc"
 WORK_DIR="/tmp/${USER}-brewstrap"
 HOMEBREW_URL="http://raw.github.com/mxcl/homebrew/go"
-RVM_URL="https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer"
-RVM_MIN_VERSION="185"
-RBENV_RUBY_VERSION="1.9.3-p125"
-RVM_RUBY_VERSION="ruby-1.9.3-p125"
+RVM_URL="https://get.rvm.io"
+RVM_MIN_VERSION="1.15.9"
+RVM_RUBY_VERSION="ruby-1.9.3-p194"
 CHEF_MIN_VERSION="10.12.0"
 CHEF_LIBRARIAN_MIN_VERSION="0.0.24"
 DARWIN_VERSION=`/usr/bin/sw_vers -productVersion | sed 's/\(^[0-9]*\)\.\([0-9]*\)\(\..*\)/\1_\2/'`
@@ -38,11 +37,9 @@ GIT_PASSWORD_SCRIPT="${WORK_DIR}/retrieve_git_password.sh"
 RUBY_RUNNER=""
 RUBY_GEM_OPTIONS="--no-ri --no-rdoc"
 USING_RVM=0
-USING_RBENV=0
 TOTAL=14
 STEP=1
 XCODE=false
-RVM=true
 clear
 
 GIT_DEBUG=""
@@ -126,13 +123,12 @@ function attempt_to_download_osx_gcc_installer() {
 }
 
 function add_user_to_sudoers() {
-
   (sudo grep "^${USER}[[:blank:]]*ALL=(ALL)[[:blank:]]*NOPASSWD\:[[:blank:]]*ALL[[:blank:]]*$" /etc/sudoers 1>/dev/null 2>&1)
   if [ "$?" -ne "0" ]; then
+    echo -e "Current user: ${USER} is not found in the sudoers file.  Adding NOPASSWD entry now."
     (sudo lockfile -r 0 /etc/sudoers.tmp 1>/dev/null 2>&1)
     if [ "$?" -ne "0" ]; then
-      echo "/etc/sudoers.tmp exists.  This usually indicates that visudo is in use."
-      exit 1
+      print_error "/etc/sudoers.tmp exists.  This usually indicates that visudo is in use."
     else
       sudo cp /etc/sudoers /tmp/sudoers.new
       sudo chmod u+w /tmp/sudoers.new
@@ -145,10 +141,9 @@ function add_user_to_sudoers() {
         sudo chown root:wheel /tmp/sudoers.new
         sudo mv /tmp/sudoers.new /etc/sudoers
       else
-        echo "visudo found a problem with the edits made."
         sudo rm /etc/sudoers.tmp
         sudo rm /tmp/sudoers.new
-        exit 1
+        print_error "visudo found a problem with the edits made."
       fi
       sudo rm /etc/sudoers.tmp
     fi
@@ -186,34 +181,34 @@ if [ -d /tmp/chef ]; then
   rm -rf /tmp/chef && ln -s ${WORK_DIR}/chef /tmp/chef
 fi
 
-#print_step "Collecting information.."
-#if [ -z $GITHUB_LOGIN ]; then
-#  echo -n "Github Username: "
-#  stty echo
-#  read GITHUB_LOGIN
-#  echo ""
-#fi
-#
-#if [ -z $GITHUB_PASSWORD ]; then
-#  echo -n "Github Password: "
-#  stty -echo
-#  read GITHUB_PASSWORD
-#  echo ""
-#fi
-#
-#if [ -z $CHEF_REPO ]; then
-#  echo -n "Chef Repo (Take the github HTTP URL): "
-#  stty echo
-#  read CHEF_REPO
-#  echo ""
-#fi
-#stty echo
-#
-#rm -f $BREWSTRAPRC
-#echo "GITHUB_LOGIN=${GITHUB_LOGIN}" >> $BREWSTRAPRC
-#echo "GITHUB_PASSWORD=${GITHUB_PASSWORD}" >> $BREWSTRAPRC
-#echo "CHEF_REPO=${CHEF_REPO}" >> $BREWSTRAPRC
-#chmod 0600 $BREWSTRAPRC
+print_step "Collecting information.."
+if [ -z $GITHUB_LOGIN ]; then
+  echo -n "Github Username: "
+  stty echo
+  read GITHUB_LOGIN
+  echo ""
+fi
+
+if [ -z $GITHUB_PASSWORD ]; then
+  echo -n "Github Password: "
+  stty -echo
+  read GITHUB_PASSWORD
+  echo ""
+fi
+
+if [ -z $CHEF_REPO ]; then
+  echo -n "Chef Repo (Take the github HTTP URL): "
+  stty echo
+  read CHEF_REPO
+  echo ""
+fi
+stty echo
+
+rm -f $BREWSTRAPRC
+echo "GITHUB_LOGIN=${GITHUB_LOGIN}" >> $BREWSTRAPRC
+echo "GITHUB_PASSWORD=${GITHUB_PASSWORD}" >> $BREWSTRAPRC
+echo "CHEF_REPO=${CHEF_REPO}" >> $BREWSTRAPRC
+chmod 0600 $BREWSTRAPRC
 
 if [ ! -e /usr/local/bin/brew ]; then
   print_step "Installing homebrew"
@@ -258,7 +253,7 @@ if [ ! -e /usr/bin/gcc ]; then
       print_step "Xcode already installed"
     fi
   else
-    print_step "There is no GCC available, installing the OSX GCC tools. If you want XCode instead, re-run this script with XCODE=true"
+    print_step "There is no GCC available, installing the OSX GCC tools."
     print_step "Installing OSX GCC Installer from package..."
     if [ ! -e ${WORK_DIR}/$OSX_GCC_INSTALLER_NAME ]; then
       attempt_to_download_osx_gcc_installer
@@ -301,114 +296,68 @@ if [ ! -e /usr/bin/g++-4.2 ]; then
   sudo ln -fs /usr/bin/g++ /usr/bin/g++-4.2
 fi
 
-echo "Got past git install"
-exit 0
-
-if $RVM ; then
-  RUBY_RUNNER="rvm ${RVM_RUBY_VERSION} exec"
-  if [ ! -e /usr/local/rvm/bin/rvm ]; then
-    print_step "Installing RVM"
-    bash -s stable < <( curl -fsSL ${RVM_URL} )
-    if [ ! $? -eq 0 ]; then
-      print_error "Unable to install RVM!"
-    fi
-  else
-    RVM_VERSION=`~/.rvm/bin/rvm --version | cut -f 2 -d ' ' | head -n2 | tail -n1 | sed -e 's/\.//g'`
-    if [ "${RVM_VERSION}0" -lt "${RVM_MIN_VERSION}0" ]; then
-      print_step "RVM needs to be upgraded..."
-      ~/.rvm/bin/rvm get 1.8.5
-    else
-      print_step "RVM already installed"
-    fi
+RUBY_RUNNER="rvm ${RVM_RUBY_VERSION} exec"
+if [ ! -e /usr/local/rvm/bin/rvm ]; then
+  print_step "Installing RVM"
+  sudo bash -s stable < <( curl -fsSL ${RVM_URL} )
+  if [ ! $? -eq 0 ]; then
+    print_error "Unable to install RVM!"
   fi
-
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
-  if [ ! -e ~/.bash_profile ]; then
-      echo "[[ -s \"\$HOME/.rvm/scripts/rvm\" ]] && source \"\$HOME/.rvm/scripts/rvm\"" > ~/.bash_profile
-  fi
-
-  rvm list | grep ${RVM_RUBY_VERSION}
-  if [ $? -gt 0 ]; then
-    print_step "Installing RVM Ruby ${RVM_RUBY_VERSION}"
-    gcc --version | head -n1 | grep llvm >/dev/null
-    if [ $? -eq 0 ]; then
-      export CC="gcc-4.2"
-    fi
-    rvm install ${RVM_RUBY_VERSION}
-    unset CC
-    if [ ! $? -eq 0 ]; then
-      print_error "Unable to install RVM ${RVM_RUBY_VERSION}"
-    fi
-  else
-    print_step "RVM Ruby ${RVM_RUBY_VERSION} already installed"
-  fi
-  USING_RVM=1
 else
-  if [ ! -d ~/.rvm ]; then
-    if [ ! -d /usr/local/rvm ]; then
-      print_step "Found no RVM on the system, installing rbenv by default. If you wish to use RVM instead, please re-run with RVM=true"
-      if [ ! -d ~/.rbenv ]; then
-        cd ~/ && git clone ${GIT_DEBUG} git://github.com/sstephenson/rbenv.git .rbenv
-      fi
-      unset GEM_PATH
-      unset GEM_HOME
-      unset MY_RUBY_HOME
-      (echo $PATH | grep "rbenv") || (test -e ~/.bash_profile  && cat ~/.bash_profile | grep PATH | grep rbenv) || false
-      if [ $? -eq 1 ]; then
-        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-      fi
-      grep "rbenv init" ~/.bash_profile
-      if [ $? -eq 1 ]; then
-        echo "eval \"\$(rbenv init -)\"" >> ~/.bash_profile
-      fi
-      export PATH=$HOME/.rbenv/bin:$PATH
-      if [ ! -d ~/.rbenv/plugins/ruby-build ]; then
-        mkdir -p ~/.rbenv/plugins && cd ~/.rbenv/plugins && git clone $GIT_DEBUG git://github.com/sstephenson/ruby-build.git
-      fi
-      if [ -e /usr/local/bin/rbenv-install ]; then
-        brew uninstall ruby-build
-      fi
-      gcc --version | head -n1 | grep llvm >/dev/null
-      if [ $? -eq 0 ]; then
-        export CC="gcc-4.2"
-      fi
-      which rbenv
-      if [ $? -eq 1 ]; then
-        print_error "Unable to find rbenv in ${PATH} !"
-        exit 1
-      fi
-      rbenv versions | grep ${RBENV_RUBY_VERSION}
-      if [ ! $? -eq 0 ]; then
-        rbenv install ${RBENV_RUBY_VERSION}
-        rbenv rehash
-        if [ ! $? -eq 0 ]; then
-          print_error "Unable to install rbenv or ruby ${RBENV_RUBY_VERSION}!"
-        fi
-      fi
-      USING_RBENV=1
-      RUBY_RUNNER=""
-      eval "$(rbenv init -)"
-      rbenv shell ${RBENV_RUBY_VERSION}
-    else
-      print_step "Found multi-user RVM installation. Skipping installation..."
-      RUBY_RUNNER="rvm ${RVM_RUBY_VERSION} exec"
-      USING_RVM=1
-    fi
+  RVM_VERSION=`/usr/local/rvm/bin/rvm --version | cut -f 2 -d ' ' | head -n2 | tail -n1`
+  if [ "${RVM_VERSION}" != "${RVM_MIN_VERSION}" ]; then
+    print_step "Current RVM version ${RVM_VERSION} differs from target ${RVM_MIN_VERSION}.  Changing version now."
+    /usr/local/rvm/bin/rvm get ${RVM_MIN_VERSION} --auto
   else
-    print_step "Found local user RVM installation. Skipping installation..."
-    RUBY_RUNNER="rvm ${RVM_RUBY_VERSION} exec"
-    USING_RVM=1
+    print_step "RVM already installed"
   fi
 fi
+
+sudo dscl localhost -read /Local/Default/Groups/rvm/ GroupMembership | grep -w "${USER}"
+if [ ! $? -eq 0 ]; then
+  sudo dscl localhost -append /Local/Default/Groups/rvm GroupMembership ${USER}
+   if [ ! $? -eq 0 ]; then
+    print_error "Unable to add ${USER} to path: /Local/Default/Groups/rvm/ key: GroupMembership"
+  fi
+fi
+
+[[ -s "/etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh"
+
+if [ ! -e ~/.bash_profile ]; then
+  echo "[[ -s \"/etc/profile.d/rvm.sh\" ]] && source \"/etc/profile.d/rvm.sh\"" > ~/.bash_profile
+fi
+
+rvm list | grep ${RVM_RUBY_VERSION}
+if [ $? -gt 0 ]; then
+  print_step "Installing RVM Ruby ${RVM_RUBY_VERSION}"
+  gcc --version | head -n1 | grep llvm >/dev/null
+  if [ $? -eq 0 ]; then
+    export CC="gcc-4.2"
+  fi
+  brew install libksba autoconf automake
+  rvm reload
+  if [ $DARWIN_VERSION != "10_6" ]; then
+
+    ${rvm_path}/bin/rvm install ${RVM_RUBY_VERSION}
+  else
+    ${rvm_path}/bin/rvm install ${RVM_RUBY_VERSION} --with-clang
+  fi
+  unset CC
+  if [ ! $? -eq 0 ]; then
+    print_error "Unable to install RVM ${RVM_RUBY_VERSION}"
+  fi
+else
+  print_step "RVM Ruby ${RVM_RUBY_VERSION} already installed"
+fi
+
+[[ -s "/etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh" && rvm use ${RVM_RUBY_VERSION} --default
+
+USING_RVM=1
+
 ${RUBY_RUNNER} gem specification --version ">=${CHEF_MIN_VERSION}" chef 2>&1 | awk 'BEGIN { s = 0 } /^name:/ { s = 1; exit }; END { if(s == 0) exit 1 }'
 if [ $? -gt 0 ]; then
   print_step "Installing chef gem"
   ${RUBY_RUNNER} gem install chef --version "${CHEF_MIN_VERSION}" ${RUBY_GEM_OPTIONS}
-  if [ $USING_RBENV -eq 1 ]; then
-    print_step "Rehashing RBEnv for chef"
-    rbenv rehash
-  fi
   if [ ! $? -eq 0 ]; then
     print_error "Unable to install chef!"
   fi
@@ -483,10 +432,6 @@ if [ -e ${WORK_DIR}/chef/Cheffile ]; then
   if [ $? -gt 0 ]; then
     print_step "Installing librarian chef gem"
     ${RUBY_RUNNER} gem install librarian ${RUBY_GEM_OPTIONS}
-    if [ $USING_RBENV -eq 1 ]; then
-      print_step "Rehasing RBEnv for librarian chef"
-      rbenv rehash
-    fi
     if [ ! $? -eq 0 ]; then
       print_error "Unable to install librarian chef!"
     fi
@@ -496,10 +441,6 @@ if [ -e ${WORK_DIR}/chef/Cheffile ]; then
   if [ -e ${WORK_DIR}/chef/Gemfile ]; then
     print_step "Installing bundler gem"
     ${RUBY_RUNNER} gem install bundler ${RUBY_GEM_OPTIONS}
-    if [ $USING_RBENV -eq 1 ]; then
-      print_step "Rehasing RBEnv for bundler"
-      rbenv rehash
-    fi
     if [ ! $? -eq 0 ]; then
       print_error "Unable to install bundler!"
     fi
@@ -527,6 +468,10 @@ CHEF_COMMAND="GITHUB_PASSWORD=$GITHUB_PASSWORD GITHUB_LOGIN=$GITHUB_LOGIN ${RUBY
 if [ ! -z ${DEBUG} ]; then
   echo $CHEF_COMMAND
 fi
+
+echo -e "Made it up to chef being ready to run"
+exit 0
+
 sudo -E env ${CHEF_COMMAND}
 if [ ! $? -eq 0 ]; then
   print_error "BREWSTRAP FAILED!"
