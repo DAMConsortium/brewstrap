786204bebcd17fb777dee65fc62cc091fde75414
Steve Houser <steve@damconsortium.com>
2012-09-27 15:45:14 -0600

    Updated to master

--- brewstrap.sh
+++ brewstrap.sh
@@ -2,7 +2,7 @@
 
 BREWSTRAP_BASE="https://github.com/DAMConsortium/brewstrap"
 PLATFORM=`uname -s`
-BREWSTRAP_BIN="${BREWSTRAP_BASE}/raw/develop/brewstrap_${PLATFORM}.sh"
+BREWSTRAP_BIN="${BREWSTRAP_BASE}/raw/master/brewstrap_${PLATFORM}.sh"
 
 curl -fsSL "${BREWSTRAP_BIN}" > /tmp/${USER}-brewstrap_${PLATFORM}.sh
 
